6a7bb0b652111b48a40b083e78001a78f8bfd35e
UtsavKalia <56361715+UtsavKalia@users.noreply.github.com>
2020-01-30 14:43:57 -0500

    add constructor and add get/set method with validation and added main class

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+public static void main(String args[]){
+
+    Student utsav = new Student("utsav","kalia",200441854);
+
+
+
+    System.out.println(utsav);
+
+
+}
+}
--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,52 @@
 public class Student {
-    
+
     private String firstName, lastName;
     private int studentNumber;
 
+    public Student(String firstName, String lastName, int studentNumber) {
+        setFirstName(firstName);
+
+        setLastName(lastName);
+
+        setStudentNumber(studentNumber);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        if (firstName.length()>2){
+            String firstNameToUpperCase = String.valueOf((firstName.toUpperCase().charAt(0)));
+        this.firstName = firstNameToUpperCase+ firstName.substring(1);}
+        else
+            throw new IllegalArgumentException("first name must be greater than 2 character");
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName.length()>2){
+            String lastNameToUpperCase = String.valueOf(lastName.toUpperCase().charAt(0));
+        this.lastName = lastNameToUpperCase + lastName.substring(1);}
+        else
+            throw new IllegalArgumentException("last name must be greater than 2 character");
+    }
+
+    public int getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(int studentNumber) {
+        if (Integer.toString(studentNumber).length() == 9){
+        this.studentNumber = studentNumber;}
+        else
+            throw new IllegalArgumentException("student number length must be 9 ");
+    }
+
+    public String toString(){
+        return String.format("%s %s student# %d",firstName,lastName,studentNumber);
+    }
 }
