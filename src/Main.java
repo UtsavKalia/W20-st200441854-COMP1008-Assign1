32ca0c7faa98b9882325fb7cd15b8bf82f82203a
UtsavKalia <56361715+UtsavKalia@users.noreply.github.com>
2020-01-31 13:32:39 -0500

    Added the view, controller and launch

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 public static void main(String args[]){
-
+launch(args);
     Student utsav = new Student("utsav","kalia",200441854);
-
-
-
     System.out.println(utsav);
-
-
 }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("StudentCard.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Student Card");
+        primaryStage.show();
+    }
 }
